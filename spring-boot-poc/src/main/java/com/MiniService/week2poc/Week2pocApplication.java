4948a55e002a76afd1252b0971961ad0d000350f
Mansi Waghmode <mwaghmode1805@gmail.com>
2026-01-28 13:20:25 +0530

    Add Spring Boot POC

---
+++ spring-boot-poc/src/main/java/com/MiniService/week2poc/Week2pocApplication.java
@@ -0,0 +1,13 @@
+package com.MiniService.week2poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week2pocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week2pocApplication.class, args);
+	}
+
+}
